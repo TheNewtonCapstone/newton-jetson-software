ee4af6c8f7896ac41ad304efacb48f1f46b68070
shamiivan <shamiivan@gmail.com>
2024-12-11 21:11:19 -0500

    fix (logger) : added missing #pragma once

--- utils/result.h
+++ utils/result.h
@@ -1,47 +1,73 @@
+// result.h
 #pragma once
 #include <string>
 #include "utils/logger.hpp"
 
+// Forward declaration of the template
+template<typename T>
+class result;
+
+// Main template declaration
 template<typename T>
 class result {
 public:
   static result<T> success(T value) {
     return result<T>(value, "");
   }
+
   static result<T> error(std::string tag, std::string error_msg) {
-    Logger::ERROR(tag, error_msg);
+    Logger::ERROR(tag.c_str(), "%s", error_msg.c_str());
     return result<T>(T(), error_msg);
   }
 
-  bool has_error() const;
-  bool is_success() const;
-  T get_value() const;
-  std::string get_error_msg() const;
+  bool has_error() const {
+    return !error_msg.empty();
+  }
+
+  bool is_success() const {
+    return error_msg.empty();
+  }
+
+  T get_value() const {
+    if (has_error()) {
+      Logger::ERROR("result", "Error Accessing value in error state %s", error_msg.c_str());
+    }
+    return value;
+  }
+
+  std::string get_error_msg() const {
+    return error_msg;
+  }
 
 private:
-  result(T value, std::string error_msg);
+  result(T value, std::string error_msg)
+    : value(std::move(value)), error_msg(std::move(error_msg)) {
+  }
+
 private:
   T value;
   std::string error_msg;
 };
 
+// Void specialization declaration
 template<>
 class result<void> {
 public:
-  static result<void> success() {
-    return result<void>("");
-  }
-
+  static result<void> success();
   static result<void> error(std::string tag, std::string error_msg) {
-    Logger::ERROR(tag.c_str(), error_msg.c_str());
+    Logger::ERROR(tag.c_str(), "%s", error_msg.c_str());
     return result<void>(error_msg);
   }
 
-  bool has_error() const;
-
-  bool is_success() const;
-
-  std::string get_error_msg() const;
+  bool has_error() {
+    return !error_msg.empty();
+  };
+  bool is_success() {
+    return error_msg.empty();
+  }
+  std::string get_error_msg() {
+    return error_msg;
+  }
 
 private:
   explicit result(std::string error_msg) : error_msg(std::move(error_msg)) {}
