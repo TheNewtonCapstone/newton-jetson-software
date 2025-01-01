bda761a1a590bc1c29b7165e7f80bf555b061de7
shamiivan <shamiivan@gmail.com>
2024-12-31 20:03:07 -0500

    ref : changed result class to use factory functions instead of static functions!

--- utils/result.h
+++ utils/result.h
@@ -9,29 +9,38 @@
  * */
 
 template<typename T>
+class result;
+
+template<typename T>
+result<T> sucess(T value);
+
+template<typename T>
+result<T> error(std::string error_msg);
+
+inline result<void> success();
+inline result<void> error(std::string error_msg);
+
+
+template<typename T>
 class result {
 public:
-  static result<T> success(T value) {
-    return result<T>(value, "");
-  }
-
-  static result<T> error(std::string error_msg) {
-    return result<T>(T(), error_msg);
-  }
+  friend result<T> success<T>(T value);
+  friend result<T> error<T>(std::string error_msg);
 
   bool has_error() const {
     return !error_msg.empty();
   }
 
   T get_value() const {
-    if (has_error()) {
-      Logger::ERROR("result", "Error Accessing value in error state %s", error_msg.c_str());
-    }
     return value;
   }
 
   std::string get_error_msg() const {
     return error_msg;
+  }
+
+  T value_or(T default_value) const {
+    return has_error() ? default_value : value;
   }
 
 private:
@@ -44,16 +53,24 @@ private:
   std::string error_msg;
 };
 
-// Void specialization declaration
+template<typename T>
+result<T> success(T value) {
+  return result<T>(value, "");
+}
+
+template <typename T>
+result<T> error(std::string error_msg) {
+  return result<T>(T(), error_msg);
+}
+
+
+
+/* Void returning functions need a sepecial declaration  */
 template<>
 class result<void> {
 public:
-  static result<void> success() {
-    return result<void>("");
-  }
-  static result<void> error(std::string error_msg) {
-    return result<void>(error_msg);
-  }
+  friend result<void> success();
+  friend result<void> error(std::string error_msg);
 
   bool has_error() const {
     return !error_msg.empty();
@@ -69,3 +86,11 @@ private:
 private:
   std::string error_msg;
 };
+
+inline result<void> success() {
+  return result<void>("");
+}
+
+inline result<void> error(std::string error_msg) {
+  return result<void>(error_msg);
+}
