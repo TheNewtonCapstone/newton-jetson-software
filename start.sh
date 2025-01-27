c05f589e5dfa78ca6e30d5aa54bc8f97e5eba8b2
The Newton Capstone <the.newton.capstone@gmail.com>
2025-01-27 13:04:59 -0500

    imu module feature addition, refer to readme for usage instructions

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-
-exec docker compose up -d
+# !/bin/bash
+work_dir=$(pwd)
+# exec docker compose up -d
+function run_container(){
+    docker run -it --net=host \
+        -e DISPLAY=$DISPLAY \
+        -v /dev:/dev \
+        --device-cgroup-rule='c *:* rmw' \
+        --device=/dev \
+        -v /${work_dir}:/${work_dir}\
+        thenewtoncapstone/newton:l4t-r36.4.0 \
+    bin/bash
+}
