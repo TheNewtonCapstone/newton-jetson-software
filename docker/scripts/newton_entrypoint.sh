7d19ab6505cc016ad9521e153ba13acaac8fd769
shamiivan <shamiivan@gmail.com>
2025-02-08 16:25:59 -0500

    feat: add Dockerfile and configuration scripts for ONNX Runtime setup

--- docker/scripts/newton_entrypoint.sh
+++ docker/scripts/newton_entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 
 # setup ros2 environment
 source /opt/ros/"$ROS_DISTRO"/setup.bash --
-source ~/ros2_ws/install/setup.bash --
 
 # add sourcing to .bashrc
 echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
