49cbc3aaa5162056b047f8496cd6a1b2b30b167e
The Newton Capstone <the.newton.capstone@gmail.com>
2025-02-08 20:03:56 -0500

    feat: activate virtual environment in setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@ ROOT=$(pwd)
 export NEWTON_ROOT="${ROOT}"
 export JETSON_CONTAINERS_PATH="${ROOT}/lib/jetson-containers"
 export PYTHONPATH="${ROOT}/:${PYTHONPATH}"
+VENV_PATH="$ROOT"/.venv
+
 echo $ROOT
 INSTALL_PREFIX="/usr/local/bin"
 
 # Make newton executable and create symlink
 chmod +x "$ROOT/newton"
 sudo ln -sf "$ROOT/newton" "$INSTALL_PREFIX/newton"
+source "$VENV_PATH"/bin/activate
