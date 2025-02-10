6d52d98ee67ddc209db28ccc698b3abd5d517d28
shamiivan <shamiivan@gmail.com>
2025-02-09 21:58:37 -0500

    fix: update newton_entrypoint.sh to add alias for newton.py

--- docker/scripts/newton_entrypoint.sh
+++ docker/scripts/newton_entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # shellcheck disable=SC1090,SC1091
-set -e
+# set -e
 
 # setup ros2 environment
 source /opt/ros/"$ROS_DISTRO"/setup.bash --
 
 # add sourcing to .bashrc
 echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
-echo ". '/home/newton/projects/newton-jetson-software/setup.bash'" >> ~/.bashrc
 
-exec "$@"
+ROOT=$1
+# Execute the command with stdin attached
+if [ $# -gt 0 ]; then
+  # Print the arguments
+  echo "alias nt='python3 $ROOT/newton.py'" >> ~/.bashrc
+fi
+exec bash
