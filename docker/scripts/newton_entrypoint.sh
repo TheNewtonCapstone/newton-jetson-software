f5ab506eb1633748ffe3d6ccf8e7ca722fe3f7e0
shamiivan <shamiivan@gmail.com>
2025-02-12 01:28:37 -0500

    fix: update newton_entrypoint.sh to add greeting and ensure correct ROS setup sourcing

--- docker/scripts/newton_entrypoint.sh
+++ docker/scripts/newton_entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC1090,SC1091
 # set -e
+echo "Hello Docker!"
 
 # setup ros2 environment
 source /opt/ros/"$ROS_DISTRO"/setup.bash --
+source /opt/ros/humble/setup.bash
 
 # add sourcing to .bashrc
 echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
