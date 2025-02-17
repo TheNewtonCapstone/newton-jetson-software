f468fecea24e82d295d988e1f09707cc4917f85e
shamiivan <shamiivan@gmail.com>
2025-02-17 14:22:50 +0000

    ref: clean up newton_entrypoint.sh by removing redundant ROS setup commands

--- docker/scripts/newton_entrypoint.sh
+++ docker/scripts/newton_entrypoint.sh
@@ -4,9 +4,6 @@
 echo "Hello Docker!"
 
 ROS_DISTRO=humble
-# setup ros2 environment
-source /opt/ros/"$ROS_DISTRO"/setup.bash --
-source /opt/ros/humble/setup.bash
 
 # add sourcing to .bashrc
 echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
