8e7f567dad9b0ee3139b35c380f2f5ee0df801de
shamiivan <shamiivan@gmail.com>
2025-03-30 17:46:45 -0400

    refactor: improve logging and update method in HarmonicGait class

--- src/n_controller/src/gaits/harmonic_gait.cpp
+++ src/n_controller/src/gaits/harmonic_gait.cpp
@@ -1,36 +1,45 @@
 #include "gaits/harmonic_gait.h"
-#include "logger.h"
 
 #include <rclcpp/rclcpp.hpp>
+
+#include "logger.h"
 
 using namespace newton;
 
 HarmonicGait::HarmonicGait(const rclcpp::NodeOptions &options)
-    : BaseGait("harmonic_gait", true, options)
-{
-  
+    : BaseGait("harmonic_gait", true, options) {
   Logger::get_instance().set_logfile("harmonic_gait.csv");
 
-  std::string log_title ="time,";
-  log_title += "hfe_offset,kfe_offset,"; 
-  log_title += "target_fl_hfe,target_fl_kfe,target_fr_hfe,target_fr_kfe,target_hl_hfe,target_hl_kfe,target_hr_hfe,target_hr_kfe,";
-  log_title += "pos_fl_hfe,pos_fl_kfe,pos_fr_hfe,pos_fr_kfe,pos_hl_hfe,pos_hl_kfe,pos_hr_hfe,pos_hr_kfe,";
-  log_title += "vel_fl_hfe,vel_fl_kfe,vel_fr_hfe,vel_fr_kfe,vel_hl_hfe,vel_hl_kfe,vel_hr_hfe,vel_hr_kfe,";
-  log_title += "tor_fl_hfe,tor_fl_kfe,tor_fr_hfe,tor_fr_kfe,tor_hl_hfe,tor_hl_kfe,tor_hr_hfe,tor_hr_kfe,";
+  std::string log_title = "time,";
+  log_title += "hfe_offset,kfe_offset,";
+  log_title +=
+      "target_fl_hfe,target_fl_kfe,target_fr_hfe,target_fr_kfe,target_hl_hfe,"
+      "target_hl_kfe,target_hr_hfe,target_hr_kfe,";
+  log_title +=
+      "pos_fl_hfe,pos_fl_kfe,pos_fr_hfe,pos_fr_kfe,pos_hl_hfe,pos_hl_kfe,pos_"
+      "hr_hfe,pos_hr_kfe,";
+  log_title +=
+      "vel_fl_hfe,vel_fl_kfe,vel_fr_hfe,vel_fr_kfe,vel_hl_hfe,vel_hl_kfe,vel_"
+      "hr_hfe,vel_hr_kfe,";
+  log_title +=
+      "tor_fl_hfe,tor_fl_kfe,tor_fr_hfe,tor_fr_kfe,tor_hl_hfe,tor_hl_kfe,tor_"
+      "hr_hfe,tor_hr_kfe,";
   Logger::INFO("harmonic_gait", log_title.c_str());
 
   BaseGait::init();
 };
 
-result<void> HarmonicGait::move()
-{
+std::array<float, NUM_JOINTS> HarmonicGait::update(
+    const std::array<float, GaitManager::NUM_OBSERVATIONS> &observations) {
   std::string log_line = "";
 
-  // every 5s, the amplitude will change and after 5 amplitude changes, the frequency will change (up to 5 changes)
+  // every 5s, the amplitude will change and after 5 amplitude changes, the
+  // frequency will change (up to 5 changes)
   const float INITIAL_AMPLITUDE = 0.5;
   const float INITIAL_FREQUENCY = 0.3;
 
-  // every 5s, the amplitude will change and after 5 amplitude changes, the frequency will change (up to 5 changes)
+  // every 5s, the amplitude will change and after 5 amplitude changes, the
+  // frequency will change (up to 5 changes)
   static float base_amplitude = INITIAL_AMPLITUDE;
   static float base_frequency = INITIAL_FREQUENCY;
   static int amplitude_changes = 0;
@@ -42,8 +51,7 @@ result<void> HarmonicGait::move()
   auto current_time = now.seconds();
 
   // Check if 5 seconds have passed since the last change
-  if (current_time - last_change_time >= 60.0)
-  {
+  if (current_time - last_change_time >= 60.0) {
     // Update the last change time
     last_change_time = current_time;
 
@@ -54,21 +62,18 @@ result<void> HarmonicGait::move()
     RCLCPP_INFO(this->get_logger(), "Amplitude changed to %f", base_amplitude);
 
     // If we've changed amplitude 5 times, change frequency
-    if (amplitude_changes >= 5)
-    {
+    if (amplitude_changes >= 5) {
       amplitude_changes = 0;
       base_amplitude = INITIAL_AMPLITUDE;
       frequency_changes++;
 
-      // RCLCPP_INFO(this->get_logger(), "Frequency changed to %f", base_frequency);
+      // RCLCPP_INFO(this->get_logger(), "Frequency changed to %f",
+      // base_frequency);
 
       // Increase frequency (up to 5 changes)
-      if (frequency_changes < 5)
-      {
+      if (frequency_changes < 5) {
         base_frequency += 0.1;
-      }
-      else
-      {
+      } else {
         frequency_changes = 0;
         base_frequency = INITIAL_FREQUENCY;
       }
@@ -80,8 +85,9 @@ result<void> HarmonicGait::move()
   float frequency = base_frequency;
 
   // Simple approach if standing positions are close to 0
-  float max_amplitude = M_PI/2; // Maximum desired range is from -π/2 to π/2
-  float hfe_scaling = std::min(1.0f, max_amplitude / amplitude); // Scale down if needed
+  float max_amplitude = M_PI / 2;  // Maximum desired range is from -π/2 to π/2
+  float hfe_scaling =
+      std::min(1.0f, max_amplitude / amplitude);  // Scale down if needed
 
   float base_position = amplitude * sin(TWO_PI * frequency * now.seconds());
   float hfe_offset = amplitude * hfe_scaling * base_position;
@@ -91,10 +97,9 @@ result<void> HarmonicGait::move()
   log_line += std::to_string(kfe_offset) + ",";
 
   std::array<float, NUM_JOINTS> positions{};
-  
+
   // Calculate target positions for all joints
-  for (auto &leg : leg_ids)
-  {
+  for (auto &leg : leg_ids) {
     std::string leg_name = leg.first;
     std::array<int, 2> ids = leg.second;
 
@@ -104,37 +109,21 @@ result<void> HarmonicGait::move()
   }
 
   // Check if test should end
-  if (amplitude_changes == 5 && frequency_changes == 5)
-  {
+  if (amplitude_changes == 5 && frequency_changes == 5) {
     rclcpp::shutdown();
   }
 
-  // Log target positions
-  for (int i = 0; i < NUM_JOINTS; i++)
-  {
+  // logger
+  // Log observations
+  for (int i = 0; i < NUM_OBSERVATIONS; i++) {
+    log_line += std::to_string(observations[i]) + ",";
+  }
+  /// log target positions
+  for (int i = 0; i < NUM_JOINTS; i++) {
     log_line += std::to_string(positions[i]) + ",";
   }
-
-  // Log actual positions
-  for (int i = 0; i < NUM_JOINTS; i++)
-  {
-    log_line += std::to_string(joints[i].curr_pos) + ",";
-  }
-
-  // Log velocities
-  for (int i = 0; i < NUM_JOINTS; i++)
-  {
-    log_line += std::to_string(joints[i].curr_vel) + ",";
-  }
-
-  // Log torques
-  for (int i = 0; i < NUM_JOINTS; i++)
-  {
-    log_line += std::to_string(joints[i].curr_torque) + ((i < NUM_JOINTS - 1) ? "," : "");
-  }
-
   // Send target positions to motors
-  set_joints_position(positions);
+  return positions;
 
   // Write to log file
   Logger::INFO("harmonic_gait", log_line.c_str());
